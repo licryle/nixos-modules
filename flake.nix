a499bfc19a7d6ed8274bcebd20bfbb4c969fe690
licryle <cyrille@berliat.fr>
2026-07-03 21:09:50 +0800

    Add overall flake for auto inclusion of sub flakes & modules.

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "Licryle's collection of reusable NixOS modules";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    import-tree.url = "github:vic/import-tree";
+  };
+
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+    inputs.import-tree ./modules
+  );
+}

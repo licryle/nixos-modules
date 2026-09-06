69cfc71a7009cbc691166f52b5aca7ec0b855345
licryle <cyrille@berliat.fr>
2026-09-06 15:56:05 +0800

    Move away from import-tree to a more granular fileset include so we can have some sub projects with custom direnv flakes.

--- flake.nix
+++ flake.nix
@@ -4,10 +4,24 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    import-tree.url = "github:vic/import-tree";
   };
 
-  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (
-    inputs.import-tree ./modules
-  );
+  outputs = inputs@{ nixpkgs, flake-parts, ... }:
+    let
+      lib = nixpkgs.lib;
+
+      # 1. Gather all files under ./modules
+      # 2. Exclude the entire direnv directory
+      # 3. Filter strictly for .nix files
+      allModuleFiles = lib.fileset.toList (
+        lib.fileset.difference ./modules (
+          lib.fileset.maybeMissing ./modules/paseo-with-pi/direnv
+        )
+      );
+      
+      nixFiles = builtins.filter (p: lib.hasSuffix ".nix" (toString p)) allModuleFiles;
+    in
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = nixFiles;
+    };
 }
